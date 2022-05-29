d2a288d70616847dd7c3efd538b916e5a894270d
tarakuds <37577346+tarakuds@users.noreply.github.com>
2022-05-29 23:12:27 +0100

    Delete main.py

--- Reading-Text-Files/main.py
+++
@@ -1,7 +0,0 @@
-# Read text from a file, and count the occurence of words in that text
-# Example:
-# count_words("The cake is done. It is a big cake!") 
-# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
-
-
-  
