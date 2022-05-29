0f430cd5e08e8f53e65aee1a9c86778681ce434e
tarakuds <37577346+tarakuds@users.noreply.github.com>
2022-05-29 23:06:55 +0100

    updated anagram code

--- Finding-Anagrams/main.py
+++ Finding-Anagrams/main.py
@@ -3,9 +3,30 @@
 # find_anagrams("hello", "check") --> False
 # find_anagrams("below", "elbow") --> True
 
+word = input('Input the first word to compare :- ')
+anagram = input('Input the first word to compare :- ')
 
 def find_anagram(word, anagram):
     # [assignment] Add your code here
 
-    return True
+    word = word.lower()
+    anagram = anagram.lower()
+
+    if(len(word) == len(anagram)):
+        sorted_word = sorted(word)
+        sorted_anagram = sorted(anagram)
+
+        
+        if(sorted_word == sorted_anagram):
+            print("True " + word + " and " + anagram + " are anagram.")
+        else:
+            print("False " + word + " and " + anagram + " have same word length but are not anagram.")
+        return True
+
+    else:
+        print("False " + word + " and " + anagram + " are not anagram.")
+
+        return False
+
+find_anagram(word, anagram)
 
