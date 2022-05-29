ff800e8af29feba065691fa5bb94f4d6bda01f06
tarakuds <taradebby@gmail.com>
2022-05-29 23:00:12 +0100

    anagram code update

--- Reading-Text-Files/main.py
+++ Reading-Text-Files/main.py
@@ -3,23 +3,39 @@
 # count_words("The cake is done. It is a big cake!") 
 # --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
 
+import os
+
+working_directory = os.getcwd()
+print(os.getcwd())
+
 def read_file_content(filename):
     # [assignment] Add your code here 
-#    fileOpen = open('story.txt', 'r')
-#    viewFile = fileOpen.read()
-#    fileOpen.close()
+  file_directory = "c:/Users/John's Love/Documents/Zuri/PythonProject1/Reading-Text-Files/testing.txt"
 
-   with open('story.txt') as view:
-    mainContent = view.read()
-    print(mainContent)
-    
-    return "Hello World"
+  file_to_open= open(file_directory, 'r')
+  print(file_to_open.read())
+  file_to_open.close()
+  return file_directory
 
+read_file_content("testing.txt")
     
 
 
 def count_words():
-    text = read_file_content("./story.txt")
+    text = read_file_content("./testing.txt")
     # [assignment] Add your code here
+    # fname = input("Enter file name: ")
+    word=input("Enter word to be searched:")
+    k = 0
+    
+    with open(text, 'r') as f:
+        for line in f:
+          words = line.split()
+          for i in words:
+              if(i==word):
+                  k=k+1
+    print("Occurrences of the word: " + str(k))
 
     return {"as": 10, "would": 20}
+
+count_words()
