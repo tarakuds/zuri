c6ff627ae4c0edb2e9808eb22ab99b60325ee363
tarakuds <taradebby@gmail.com>
2022-05-30 19:36:23 +0100

    new commit

--- Reading-Text-Files/main.py
+++ Reading-Text-Files/main.py
@@ -3,23 +3,22 @@
 # count_words("The cake is done. It is a big cake!") 
 # --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
 
-def read_file_content(filename):
-    # [assignment] Add your code here 
-#    fileOpen = open('story.txt', 'r')
-#    viewFile = fileOpen.read()
-#    fileOpen.close()
 
-   with open('story.txt') as view:
-    mainContent = view.read()
-    print(mainContent)
-    
+def read_file_content(filename):
+    # [assignment] Add your code here     
+    fileOpen = open("c:/Users/John's Love/Documents/Zuri/PythonProject1/Reading-Text-Files/testing.txt", "r")
+   
+    viewFile = fileOpen.read()
+    fileOpen.close()
+    print(viewFile)
+        
     return "Hello World"
 
-    
+read_file_content('testing.txt')   
 
 
 def count_words():
-    text = read_file_content("./story.txt")
+    text = read_file_content("./testing.txt")
     # [assignment] Add your code here
 
     return {"as": 10, "would": 20}
